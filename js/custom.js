4f1549d5ff48be31e690007ac8b798913e122a7f
MarketSpace11 <134568340+MarketSpace11@users.noreply.github.com>
2024-11-03 12:56:39 -0800

    custom.js

--- js/custom.js
+++ js/custom.js
@@ -35,7 +35,7 @@ $(".owl-carousel").owlCarousel({
 
 function myMap() {
     var mapProp = {
-        center: new google.maps.LatLng(40.712775, -74.005973),
+        center: new google.maps.LatLng(27.94752, -82.45843),
         zoom: 18,
     };
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
